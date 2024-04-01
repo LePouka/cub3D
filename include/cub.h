a11158b69dd9fa3949c58bad9a6fa897bf290ea1
Ronen SHAY <rshay@bess-f2r2s8.clusters.42paris.fr>
2024-04-01 16:04:52 +0200

    untextured raycasting

--- include/cub.h
+++ include/cub.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rtissera <rtissera@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 19:07:29 by rtissera          #+#    #+#             */
-/*   Updated: 2024/03/31 19:07:31 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/04/01 15:36:06 by rshay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,36 @@
 /******************************************************************************/
 /*   STRUCTURES                                                               */
 /******************************************************************************/
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		l;
+    int		bits_per_pixel;
+    int		line_length;
+	int		endian;
+}           t_data;
+
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+	t_data	*img;
+}			t_vars;
+
+typedef struct s_rays
+{
+	double posX;
+	double posY;
+	double dirX;
+	double dirY;
+	double planeX;
+	double planeY;
+
+}			t_rays;
+
 
 /******************************************************************************/
 /*   FUNCTIONS                                                                */
--- src/main.c
+++ src/main.c
@@ -3,17 +3,184 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/19 20:02:02 by rtissera          #+#    #+#             */
-/*   Updated: 2024/03/19 21:43:05 by rtissera         ###   ########.fr       */
+/*   Created: 2024/03/28 15:58:09 by lebronen          #+#    #+#             */
+/*   Updated: 2024/04/01 16:02:46 by rshay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub.h"
 
-int	main()
+#define screenWidth 1920
+#define screenHeight 1080
+#define mapWidth 24
+#define mapHeight 24
+
+#define abs(x) x >= 0 ? x : -x
+
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	ft_dprintf(1, "Hello, world!\n");
-	return (0);
+	char	*dst;
+
+	dst = data->addr + (y * data->l + x * (data->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
+void init(t_data *img, void *mlx, void *mlx_win, t_rays *rays) {
+  int worldMap[mapWidth][mapHeight]=
+  {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+  };
+    img->img = mlx_new_image(mlx, screenWidth, screenHeight);
+    img->addr = mlx_get_data_addr(img->img, &img->bpp, &img->l,
+								&img->endian);
+
+    for (int x = 0; x < screenWidth; x++) {
+        //calculate ray position and direction
+        double cameraX = 2 * x / (double)screenWidth - 1; //x-coordinate in camera space
+        double rayDirX = rays->dirX + rays->planeX * cameraX;
+        double rayDirY = rays->dirY + rays->planeY * cameraX;
+              //which box of the map we're in
+      int mapX = rays->posX;
+      int mapY = rays->posY;
+
+      //length of ray from current position to next x or y-side
+      double sideDistX;
+      double sideDistY;
+
+       //length of ray from one x or y-side to next x or y-side
+      double deltaDistX = (rayDirX == 0) ? 1e30 : abs(1 / rayDirX);
+      double deltaDistY = (rayDirY == 0) ? 1e30 : abs(1 / rayDirY);
+      double perpWallDist;
+
+      //what direction to step in x or y-direction (either +1 or -1)
+      int stepX;
+      int stepY;
+
+      int hit = 0; //was there a wall hit?
+      int side; //was a NS or a EW wall hit? 
+
+      //calculate step and initial sideDist
+      if (rayDirX < 0)
+      {
+        stepX = -1;
+        sideDistX = (rays->posX - mapX) * deltaDistX;
+      }
+      else
+      {
+        stepX = 1;
+        sideDistX = (mapX + 1.0 - rays->posX) * deltaDistX;
+      }
+      if (rayDirY < 0)
+      {
+        stepY = -1;
+        sideDistY = (rays->posY - mapY) * deltaDistY;
+      }
+      else
+      {
+        stepY = 1;
+        sideDistY = (mapY + 1.0 - rays->posY) * deltaDistY;
+      }
+      //perform DDA
+      while (hit == 0)
+      {
+        //jump to next map square, either in x-direction, or in y-direction
+        if (sideDistX < sideDistY)
+        {
+          sideDistX += deltaDistX;
+          mapX += stepX;
+          side = 0;
+        }
+        else
+        {
+          sideDistY += deltaDistY;
+          mapY += stepY;
+          side = 1;
+        }
+        //Check if ray has hit a wall
+        if (worldMap[mapX][mapY] > 0) hit = 1;
+      }
+      //Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!)
+      if(side == 0) perpWallDist = (sideDistX - deltaDistX);
+      else          perpWallDist = (sideDistY - deltaDistY);
+      int lineHeight = (int)(screenHeight / perpWallDist);
+
+      //calculate lowest and highest pixel to fill in current stripe
+      int drawStart = -lineHeight / 2 + screenHeight / 2;
+      if(drawStart < 0)drawStart = 0;
+      int drawEnd = lineHeight / 2 + screenHeight / 2;
+      if(drawEnd >= screenHeight)drawEnd = screenHeight - 1;
+      for (int y = drawStart; y < drawEnd; y++) {
+        int color;
+        switch(worldMap[mapX][mapY]) {
+          case 1: color = 0xd42626; break; //red
+          case 2: color = 0x38dB4e; break; //green
+          case 3: color = 0x1927a5; break; //blue
+          case 4: color = 0xce19e2; break; //purple
+          default: color = 0xF5ed0a; break; //yellow
+          
+        }
+        if (side == 1)
+          my_mlx_pixel_put(img, x, y, color / 2);
+        else
+          my_mlx_pixel_put(img, x, y, color);
+      }
+    }
+    
+    mlx_put_image_to_window(mlx, mlx_win, img->img, 0, 0);
+
+
+}
+
+int main() {
+    void	*mlx;
+	t_data	img;
+  t_rays rays;
+    void *mlx_win;
+
+
+  double posX = 22, posY = 12;  //x and y start position
+  double dirX = -1, dirY = 0; //initial direction vector
+  double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+  rays.posX = posX;
+  rays.posY = posY;
+  rays.dirX = dirX;
+  rays.dirY = dirY;
+  rays.planeX = planeX;
+  rays.planeY = planeY;
+
+  //double time = 0; //time of current frame
+  //double oldTime = 0; //time of previous frame
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, screenWidth, screenHeight, "Hello world!");
+	init(&img, mlx, mlx_win, &rays);
+
+	mlx_loop(mlx);
+
 }
