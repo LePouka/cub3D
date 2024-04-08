c004e2c98f98823efd937c4ce5475cfa37babe75
Ronen SHAY <rshay@bess-f2r1s9.clusters.42paris.fr>
2024-04-08 14:34:21 +0200

    switch

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 11:10:41 by rshay             #+#    #+#             */
-/*   Updated: 2024/04/08 14:23:44 by rshay            ###   ########.fr       */
+/*   Updated: 2024/04/08 14:31:14 by rshay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int clavier(int keycode, t_rays *rays) {
 
-    
+    double rotate = -0.02;
 
     if (keycode == HAUT) {
         
@@ -27,21 +27,21 @@ int clavier(int keycode, t_rays *rays) {
     }
 
     if (keycode == BAS) {
-        if(rays->worldMap[(int)(rays->posX - rays->dirX * 1.2)][(int)(rays->posY)] == false)
-            rays->posX -= rays->dirX * 1.2;
-        if(rays->worldMap[(int)(rays->posX)][(int)(rays->posY - rays->dirY * 1.2)] == false)
-            rays->posY -= rays->dirY * 1.2;
+        if(rays->worldMap[(int)(rays->posX - rays->dirX * 0.8)][(int)(rays->posY)] == false)
+            rays->posX -= rays->dirX * 0.8;
+        if(rays->worldMap[(int)(rays->posX)][(int)(rays->posY - rays->dirY * 0.8)] == false)
+            rays->posY -= rays->dirY * 0.8;
         casting(rays);
     }
 
     if (keycode == DROITE) {
         //both camera direction and camera plane must be rotated
       double oldDirX = rays->dirX;
-      rays->dirX = rays->dirX * cos(-1) - rays->dirY * sin(-1);
-      rays->dirY = oldDirX * sin(-1) + rays->dirY * cos(-1);
+      rays->dirX = rays->dirX * cos(rotate) - rays->dirY * sin(rotate);
+      rays->dirY = oldDirX * sin(rotate) + rays->dirY * cos(rotate);
       double oldPlaneX = rays->planeX;
-      rays->planeX = rays->planeX * cos(-1) - rays->planeY * sin(-1);
-      rays->planeY = oldPlaneX * sin(-1) + rays->planeY * cos(-1);
+      rays->planeX = rays->planeX * cos(rotate) - rays->planeY * sin(rotate);
+      rays->planeY = oldPlaneX * sin(rotate) + rays->planeY * cos(rotate);
 
       casting(rays);
     }
