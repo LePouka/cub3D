3491a5af0de4fb2d83edb4354cd3e5f05997d5f2
Raphael TISSERAND <rtissera@bess-f4r6s3.clusters.42paris.fr>
2024-04-15 14:30:38 +0200

    readificator done

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 17:20:59 by rtissera          #+#    #+#             */
-/*   Updated: 2024/04/08 14:21:47 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/04/15 14:17:31 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ bool	parsingator(char *file_name)
 	int	fd;
 	t_map	*map;
 
-	(void)map;
 	fd = openificator(file_name);
 	if (fd == -1)
 	{
 		return (false);
 	}
+	map = readificator(argv[1]);
 	return (true);
 }
--- src/utils/readificator.c
+++ src/utils/readificator.c
@@ -6,11 +6,21 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 19:00:16 by rtissera          #+#    #+#             */
-/*   Updated: 2024/04/14 17:54:53 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/04/15 13:45:48 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
+
+char	*free_strjoin(char *s1, char *s2)
+{
+	char	*s3;
+
+	s3 = ft_strjoin(s1, s2);
+	free(s1);
+	return (s3);
+}
+
 
 t_map	read_map(int fd)
 {
@@ -23,10 +33,10 @@ t_map	read_map(int fd)
 		close_error(fd, "Cannot Read The Map\n");
 	while (line)
 	{
-		if (!c_map)
-			c_map = ft_substr(line, 0, ft_strlen(line));
-		else
+		if (c_map)
 			c_map = free_strjoin(c_map, line);
+		else
+			c_map = ft_substr(line, 0, ft_strlen(line));
 		free(line);
 		line = get_next_line(fd);
 	}
