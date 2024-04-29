a3ef60e4403483bb2007044a5519d1ef7f6814ec
Raphael TISSERAND <rtissera@paul-f4Ar2s5.clusters.42paris.fr>
2024-04-29 15:00:22 +0200

    u_int33_t

--- src/init/colorificator.c
+++ src/init/colorificator.c
@@ -6,7 +6,7 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 19:02:07 by rtissera          #+#    #+#             */
-/*   Updated: 2024/04/29 06:22:55 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/04/29 14:57:43 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 t_colord	*colorificator(t_map *map)
 {
-	t_color	*color;
+	//t_color	*color;
+	int	color;
 
 	if (strncmp("F ", map->map[5], 2) || strncmp("C ", map->map[6], 2))
 	{
@@ -23,5 +24,8 @@ t_colord	*colorificator(t_map *map)
 	color = (t_color *)malloc(sizeof(t_color *));
 	if (!color)
 		return (ft_error(strerror(errno)), NULL);
+	color = 0x00 << 4;
+	color |= 0xFF << 2;
+	color |= 0x00;
 	return (color);
 }
